ebf9f5e4df02c931f879dc404bbdfc6904d0c0f7
AnanthRK <ananthrk@ymxdata.com>
2015-03-04 14:55:19 +0530

    Fix issues in load_data

--- app/load_data.py
+++ app/load_data.py
@@ -2,7 +2,7 @@ import csv
 import sys
 from dbdriver import DB
 
-query = "INSERT INTO clickstream_data(prev_id, curr_id, num_requests, prev_name, curr_name, month) VALUES (?, ?, ?, ?, ?, ?)"
+query = "INSERT INTO clickstream_data(prev_id, curr_id, num_requests, prev_name, curr_name, month) VALUES (%s, %s, %s, %s, %s, %s)"
 vals = []
 ct = 0
 
@@ -15,11 +15,15 @@ def read_file(path):
             yield row
 
 for idx, row in enumerate(read_file(sys.argv[1])):
-    if idx % 1000 == 0:
+    if (idx > 0 and idx % 10000 == 0):
     	DB().execute_many(query, vals)
         del vals[:]
-        print 'Finished 1000 rows'
+        print 'Finished 10000 rows'
         break
+    if row[0] == '':
+    	row[0] = None
+	if row[1] == '':
+    	row[1] = None
     vals.append(tuple(row) + (month,))
     ct = ct + 1
 print "Count: ", ct
